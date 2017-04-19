6f7f02782e12ab635f5235af440983efe426ef16
xiaoqizhou <zhou_xiaoqi@hotmail.com>
2017-04-19 00:30:17 -0700

    pset_2_7c

---
+++ pyCode/pset_2_7c/main.py
@@ -0,0 +1,30 @@
+import random
+def lastNumber(n, minRandom, maxRandom):
+    residual = n
+    while residual > 0:
+        lastN = round(random.uniform(minRandom+0.5, maxRandom+0.5))
+        residual = residual - lastN
+    return lastN
+
+def game2Player(n1, n2, minRandom, maxRandom):
+    lastN1 = lastNumber(n1,minRandom, maxRandom)
+    lastN2 = lastNumber(n2,minRandom, maxRandom)
+    if lastN1 > lastN2:
+        output = 1
+    elif lastN1 < lastN2:
+        output = 2
+    else:
+        output = 0
+    return output
+
+def runGame(i):
+    secondWinN = 0
+    for n in range(1,i):
+        gameResult = game2Player(100, 200, 0, 100)
+        if gameResult == 2:
+            secondWinN = secondWinN + 1
+
+    return float(secondWinN)/i
+
+aaa = runGame(1000000)
+print(aaa)
