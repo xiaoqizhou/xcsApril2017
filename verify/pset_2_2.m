cf268f969410f081227f3bdb1c0ac07ddf818b0d
Xiaoqi Zhou <xiaoqi_zhou@apple.com>
2017-04-26 17:50:51 -0700

    pset_3 update

--- verify/pset_2_2.m
+++ verify/pset_2_2.m
@@ -1,6 +1,9 @@
 n = 100000;
 c = 2;
+result = zeros(n,c);
+output = zeros(n,5);
 for i = 1:n
+    
     cardList = 1:52;
     for j = 1:c
         a= round(rand()*size(cardList,2)+0.5);
@@ -20,6 +23,11 @@ for i = 1:n
         output(i,2) = 0;
     end
 
+    if any(result(i,:) == 1) && all(result(i,:) < 5)
+        output(i,4) = 1;
+    else
+        output(i,4) = 0;
+    end
 
     if any(result(i,:) < 5)
         output(i,3) = 1;
@@ -27,6 +35,13 @@ for i = 1:n
         output(i,3) = 0;
     end
     
+    if any(result(i,:) < 5) && all(result(i,:) < 5)
+        output(i,5) = 1;
+    else
+        output(i,5) = 0;
+    end
+    
     
 end
+
 
